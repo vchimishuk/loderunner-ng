5dda7d6896320b6597269330e91df9175b3bcee0
Viacheslav Chimishuk <vchimishuk@yandex.ru>
2024-08-18 21:48:26 +0300

    Fix bug when the runner falls from ladder with empty tile bellow.

--- game.c
+++ game.c
@@ -196,6 +196,7 @@ static void runner_tick(struct game *game, int key)
     } else if ((state == RSTATE_FALL_LEFT || state == RSTATE_FALL_RIGHT)
         || (empty_tile(game, x, y + 1)
             && !is_tile(game, x, y, MAP_TILE_ROPE)
+            && !is_tile(game, x, y, MAP_TILE_LADDER)
             && guard_at_point(game, x, y + 1) == NULL)) {
         // Continue falling process or start falling if runner is stending on
         // empty tile.
