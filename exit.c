3c4930db888275a4a5259b95884c7b615964bd00
Viacheslav Chimishuk <vchimishuk@yandex.ru>
2025-02-04 00:22:41 +0200

    Add applicaiton name prefix when printing error to stderr.

--- exit.c
+++ exit.c
@@ -9,6 +9,7 @@ void die(char *fmt, ...)
     va_list ap;
 
     va_start(ap, fmt);
+    fprintf(stderr, "loderunner-ng: ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     va_end(ap);
